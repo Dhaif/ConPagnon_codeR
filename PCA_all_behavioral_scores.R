695f00e1538dbdfab6e7809d73285374052002c6
Dhaif BEKHA <bekha.dhaif@gmail.com>
2018-08-21 16:17:45 +0200

    Add column related to other tests. Clean the code

--- PCA_all_behavioral_scores.R
+++ PCA_all_behavioral_scores.R
@@ -17,8 +17,11 @@ library(GPArotation)
 wd = "/media/db242421/db242421_data/ConPagnon_data/regression_data"
 setwd(wd)
 
+# Domain of interest
+domain <- "Alouette"
+
 # Save results
-save_results_directory <- "/media/db242421/db242421_data/ConPagnon_reports/resultsPCA/language"
+save_results_directory <- paste("/media/db242421/db242421_data/ConPagnon_reports/resultsPCA/", domain, sep = "")
 dir.create(path = save_results_directory, showWarnings = TRUE)
 
 sheet <- "ACM_resting_state_cohort"
@@ -29,23 +32,44 @@ patients_data$X__1 <- NULL
 patients_data <- as.data.frame(patients_data)
 
 # Columns name of behavioral domain of interest
-language_tests <- c("uni_deno", "plu_deno", "uni_rep", "plu_rep", 
+# TODO: Check with Lucie
+language_neel <- c("uni_deno", "plu_deno", "uni_rep", "plu_rep", 
                     "empan", "phono", "elision_i", "invers",
                     "ajout", "elision_f", "morpho", "listea", 
                     "listeb", "topo", "voc1", "voc2",
                     "voc1_ebauche", "voc2_ebauche", "abstrait_diff", "abstrait_pos",
                     "lex1", "lex2")
+
 wisc_tests <- c("wisc_sim", "wisc_voca",  "wisc_comp", "wisc_irp", 
                 "wisc_cube", "wisc_idc", "wisc_mat", "wisc_imt", 
                 "wisc_memo", "wisc_seq",  "wisc_arith", "wisc_ivt", 
                 "wisc_code", "wisc_sym")
 
+motor <- c("bbt_left_hand", "bbt_right_hand", "t9c_drmpc_g", 
+           "t9c_drrc_g", "t9c_drtt_g", "t9c_drmpc_d", 
+           "t9c_drrc_d", "t9c_drtt_d")
+
+lexical_decoding <- c("alou_tl", "alou_m", "alou_e", "alou_c",
+                      "alou_cm", "alou_ctl")
+
+excutive_functions <- c("rey_copie", "rey_dessin")
+
+
 # Subsetting the dataframe to the clinical domain of interest 
-domain_patients_data <-patients_data[, language_tests, drop = FALSE]
+domain_patients_data <-patients_data[, lexical_decoding, drop = FALSE]
+# Drop rows containing missing values
+domain_patients_data <- domain_patients_data[complete.cases(domain_patients_data), ]
 
-# Domain of interest
-domain <- "Language"
 
+# Screeplot to choose number of components
+dev.new()
+scree(rx = domain_patients_data, 
+      main = 'eigenvalues - principal component plot', 
+      factors = FALSE)
+
+
+# Number of components to keep based on Kaiser Rule: eigenvalues superior to 1.
+ncomp <- 2
 
 # Correlation between scores
 cormat <- cor(domain_patients_data)
@@ -58,20 +82,12 @@ corrplot(cormat, method = 'color',
          addgrid.col = 'black', tl.col = 'black', tl.cex = 0.75,
          mar=c(0,0,1,0))
 
-
-# Screeplot to choose number of components
-dev.new()
-scree(rx = domain_patients_data, 
-      main = 'eigenvalues - principal component plot', 
-      factors = FALSE)
-
-
-# Number of components to keep based on Kaiser Rule: eigenvalues superior to 1.
-ncomp <- 5
-
 # Perform PCA
-#res.pca <- PCA(X = domain_patients_data, scale.unit = TRUE, 
-#               graph = FALSE, ncp = ncomp)
+res.pca <- PCA(X = domain_patients_data, scale.unit = TRUE,
+              graph = FALSE, ncp = ncomp)
+
+res.pca.scores <- res.pca$ind$coord
+res.pca.scores.std <- scale(res.pca.scores, scale = TRUE, center = TRUE)
 
 # Show percentage of variance explained
 #res.pca.eig <- res.pca$eig
@@ -91,7 +107,7 @@ ncomp <- 5
 
 # interpretation of loadings: Try a rotation of principal components if desired
 # in the psych packages, computed scores are standardized
-rotation <- "oblimin"
+rotation <- "none"
 
 # Perform PCA with psych package
 # Note: The scores are standardized
@@ -159,6 +175,22 @@ write.file.csv(x = individuals.coord, f = file.path(save_results_directory,
 write.file.csv(x = res.pca.withRotation.loading, f = file.path(save_results_directory, 
                                                     paste(rotation, "_loadings.csv", sep = "")),
                row.names = TRUE)
+
+
+# Plot the results in the desired plane
+# of chosen principal components
+
+# Create a dataframe with the principal components
+# clinical variables for plotting purposes.
+
+clinical_variables <- c("Sexe", "Lesion", "langage_clinique") 
+plotting_dataframe <- merge(individuals.coord, patients_data[, clinical_variables], by = "row.names", all = TRUE)
+# Drop rows containing missing values
+plotting_dataframe <- plotting_dataframe[complete.cases(plotting_dataframe), ]
+
+
+
+
 
 
 
