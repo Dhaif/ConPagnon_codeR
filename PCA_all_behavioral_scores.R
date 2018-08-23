23af3388c4e4080096fda6eb8405fefdf3770ee4
Dhaif BEKHA <bekha.dhaif@gmail.com>
2018-08-23 17:47:36 +0200

    Assessing the loadings significance under the broken stick model. Add Boken stick model to decide PC numbers. Clean the code. Save raw scores instead of psych output if rotation is none. TODO: save fig in pdf.

--- PCA_all_behavioral_scores.R
+++ PCA_all_behavioral_scores.R
@@ -20,7 +20,7 @@ wd = "/media/db242421/db242421_data/ConPagnon_data/regression_data"
 setwd(wd)
 
 # Domain of interest
-domain <- "WISC"
+domain <- "Language (NEEL)"
 
 # Save results
 save_results_directory <- paste("/media/db242421/db242421_data/ConPagnon_reports/resultsPCA/", domain, sep = "")
@@ -42,13 +42,13 @@ language_neel <- c("uni_deno", "plu_deno", "uni_rep", "plu_rep",
                     "voc1_ebauche", "voc2_ebauche", "abstrait_diff", "abstrait_pos",
                     "lex1", "lex2")
 
-wisc_tests <- c("wisc_sim", "wisc_voca",  "wisc_comp", "wisc_irp", 
-                "wisc_cube", "wisc_idc", "wisc_mat", "wisc_imt", 
-                "wisc_memo", "wisc_seq",  "wisc_arith", "wisc_ivt", 
+wisc_tests <- c("wisc_sim", "wisc_voca",  "wisc_comp", 
+                "wisc_cube", "wisc_idc", "wisc_mat", 
+                "wisc_memo", "wisc_seq", 
                 "wisc_code", "wisc_sym")
 
 motor <- c("bbt_left_hand", "bbt_right_hand", "t9c_drtt_g", 
-           "t9c_drtt_d", "EHI")
+           "t9c_drtt_d")
 
 lexical_decoding <- c("alou_tl", "alou_m", "alou_e", "alou_c",
                       "alou_cm", "alou_ctl")
@@ -57,21 +57,13 @@ excutive_functions <- c("rey_copie", "rey_dessin")
 
 
 # Subsetting the dataframe to the clinical domain of interest 
-domain_patients_data <-patients_data[, wisc_tests, drop = FALSE]
+domain_patients_data <-patients_data[, language_neel, drop = FALSE]
 # Drop rows containing missing values
 domain_patients_data <- domain_patients_data[complete.cases(domain_patients_data), ]
 # Make sure dataframe contain numeric values only
 domain_patients_data_ <- sapply(domain_patients_data, as.numeric)
 rownames(domain_patients_data_) <- rownames(domain_patients_data)
 domain_patients_data <- domain_patients_data_
-
-
-
-# Screeplot to choose number of components
-dev.new()
-scree(rx = domain_patients_data, 
-      main = 'eigenvalues - principal component plot', 
-      factors = FALSE)
 
 # Correlation between scores
 cormat <- cor(domain_patients_data)
@@ -83,6 +75,16 @@ corrplot(cormat, method = 'color',
          number.cex = 0.5, col = col3(100), 
          addgrid.col = 'black', tl.col = 'black', tl.cex = 0.75,
          mar=c(0,0,1,0))
+
+# Choose the number of components: scree plot method,
+# percentage of variance explained, Parralel Analysis, 
+# Broken Stick model.
+
+# Screeplot to choose number of components
+dev.new()
+scree(rx = domain_patients_data, 
+      main = 'eigenvalues - principal component plot', 
+      factors = FALSE)
 
 # Number of components to keep based on Kaiser Rule: eigenvalues superior to 1, at least
 # 70 % of variance explained
@@ -103,12 +105,41 @@ fviz_eig(res.pca, addlabels = TRUE, ylim = c(0, 80),
          barfill = "orange", barcolor = "black")
 
 # Parallel Analysis
-paran(domain_patients_data, iterations = 5000, centile = 0, quietly = FALSE, 
+dev.new()
+parralel_analysis <- paran(domain_patients_data, iterations = 5000, centile = 0, quietly = FALSE, 
       status = TRUE, all = TRUE, cfa = TRUE, graph = TRUE, color = TRUE, 
       col = c("black", "red", "blue"), lty = c(1, 2, 3), lwd = 1, legend = TRUE, 
       file = "", width = 640, height = 640, grdevice = "png", seed = 0)
 
+# Broken Stick model
+# Quelles sont les valeurs propres plus grandes que la moyenne?
+ev <- res.pca$eig[,"eigenvalue"]
+ev[ev > mean(ev)]
 
+# Modele du baton brise (broken stick model)
+
+# Distribute in a random fashion the amount of variance 
+# along all the components
+n = length(ev)
+bsm = data.frame(j=seq(1:n), p=0)
+bsm$p[1] = 1/n
+for (i in 2:n) {
+  bsm$p[i] = bsm$p[i-1] + (1/(n + 1 - i))
+}
+bsm$p = 100*bsm$p/n
+
+# Dessiner les valeurs propres et le % de variance de chaque axe
+dev.new()
+par(mfrow=c(2,1))
+barplot(ev, main="Eigenvalues", col="bisque", las=2)
+abline(h=mean(ev), col="red")	# mean of eigenvalues
+legend("topright", "Mean of eigenvalues", lwd=1, col=2, bty="n")
+barplot(t(cbind(100*ev/sum(ev),bsm$p[n:1])), beside=T, 
+        main="% variance", col=c("bisque",2), las=2)
+legend("topright", c("% of variance explained", "Broken stick model"), 
+       pch=15, col=c("bisque",2), bty="n")
+
+testFA <- fac(r = domain_patients_data, nfactors = 3, rotate = "oblimin")
 
 # Correlation between variable and principal components: loadings
 #res.pca.loadings <- res.pca$var$coord
@@ -171,11 +202,94 @@ ggplot(melted_loading, aes(Var1, Var2)) +
         axis.text.x = element_text(size = 10, face = "bold", colour = "black"),
         axis.text.y = element_text(size = 8, face = "bold", colour = "black"),
         plot.title = element_text(size = 12, colour = "grey50", face = "bold")) 
+
+# Asses the significance of the Loadings
+# Compute the Peres-Neto metho.
+# See: "Giving meaningul interpretation to ordination axes: Assesing
+# loading significance in principal components analysis", 2003,
+# Ecology.
+
+# Squared the loadings matrix
+n_var <- dim(x = res.pca.withRotation.loading)[1]
+squared_loading <- res.pca.withRotation.loading**2
+
+# Compute the loadings distribution under the broken stick 
+# model
+b_k <-  data.frame(j=seq(1:n_var), p=0)
+b_k$p[1] = (1/n_var)
+
+for (i in 2:n_var) {
+  b_k$p[i] = b_k$p[i-1] + (1/(n_var + 1 - i))
+}
+b_k$p = b_k$p/n_var
+# Reverse element: the biggest proportion
+# for PC1, etc...
+b_k$p = rev(b_k$p)
+
+# Rank squared loading and assess their significance according 
+# to the borken stick model in descending order
+squared_loading_ranking <- t(apply(-squared_loading,1,rank))
+significant_loading_boolean <- cbind(squared_loading_ranking)
+
+for(var in rownames(squared_loading_ranking)){
+  # compare squared loading to expected
+  # proportion of variance under broken stick model
+  cmp <- squared_loading[var, ] > b_k$p[squared_loading_ranking[var, ]]
+  # Fill the significant matrix of loading for each variables
+  significant_loading_boolean[var, ] <- cmp
   
+}
+
+# Finaly, the raw loading under the broken
+# stick model
+significant_loading <- res.pca.withRotation.loading * significant_loading_boolean
+
+# Plot the heatmap of significant loadings
+# Reshape the data before plotting loading 
+# as heatmap with ggplot
+melted_significant_loading <- melt(significant_loading)
+
+# Plot the loading as heatmap matrix with ggplot
+dev.new()
+ggplot(melted_significant_loading, aes(Var1, Var2)) +
+  geom_tile(aes(fill = value)) + 
+  geom_text(aes(label = round(value, 3))) +
+  scale_fill_distiller(palette ="RdBu", direction = -1) +
+  theme_gray(base_size = 10) +
+  coord_flip() +
+  ylab("Principal Components") + 
+  xlab("Variables") + 
+  ggtitle(label = paste("Significant loadings under the broken stick models for ", domain, sep = " "),
+          subtitle = paste("Rotation:", rotation)) +
+  theme(axis.ticks = element_blank(),
+        panel.background = element_blank(),
+        axis.title = element_text(face = "bold", colour = "grey50"),
+        axis.text.x = element_text(size = 10, face = "bold", colour = "black"),
+        axis.text.y = element_text(size = 8, face = "bold", colour = "black"),
+        plot.title = element_text(size = 12, colour = "grey50", face = "bold")) 
+
+
+
+# Write significant loadings for easier interpretation
+write.file.csv(x = significant_loading_boolean,
+               f = file.path(save_results_directory, 
+                             paste(rotation, "_significant_loadings_interpretation.csv", sep = "")),
+               row.names = TRUE)
+
+
 # Get Correlation between scores: depending
 # rotation used, scores can be correlated if the
 # rotatation was not orthogonal.
-scores.correlation <- res.pca.withRotation$r.scores
+
+# If rotation is none save the raw scores
+# Else: save standardized output of psych packages
+if (rotation == "none") {
+  scores.correlation <- res.pca$ind$coord
+  
+}else {
+  scores.correlation <- scores.correlation <- res.pca.withRotation$r.scores
+}
+
 write.file.csv(x = scores.correlation, f = file.path(save_results_directory, 
                                                      paste(rotation, "_correlations_scores.csv", sep = "")),
                row.names = TRUE)
@@ -199,7 +313,6 @@ write.file.csv(x = res.pca.withRotation.loading, f = file.path(save_results_dire
 
 # Create a dataframe with the principal components
 # clinical variables for plotting purposes.
-
 clinical_variables <- c("Sexe", "Lesion", "langage_clinique", "cerebral_palsy", "Parole") 
 plotting_dataframe <- merge(individuals.coord, patients_data[, clinical_variables], by = 0, all = TRUE)
 # Drop rows containing missing values
@@ -207,10 +320,6 @@ plotting_dataframe <- as.data.frame(plotting_dataframe[complete.cases(plotting_d
 # Make the newly created Row.names the rownames of the dataframe
 rownames(plotting_dataframe) <- plotting_dataframe$Row.names
 plotting_dataframe$Row.names <- NULL
-
-
-
-
 
 # Draw the plot
 dev.new()
